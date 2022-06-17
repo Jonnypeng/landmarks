774695f314920a265729aa169f19c8a379c00b0e
彭江 <jiang.peng@yottacloud.cn>
2022-06-17 18:25:00 +0800

    完成地图

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by jonny on 2022/6/17.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 26.562739, longitude: 106.683887),
+        span:MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
