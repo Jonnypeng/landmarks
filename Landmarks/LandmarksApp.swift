23b047bca2c14d915de87ad84ad022a9e2b558cc
彭江 <jiang.peng@yottacloud.cn>
2022-06-17 17:13:01 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by jonny on 2022/6/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
